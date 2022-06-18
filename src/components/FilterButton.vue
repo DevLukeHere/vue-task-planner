d059a5d089fe3626d82e7c986aa00f6c3d374b6f
Luke Wong <luke@hiredly.com>
2022-06-18 17:01:46 +0800

    Create filter button component

---
+++ src/components/FilterButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="ml-4">
+    <v-btn prepend-icon="mdi-feature-search-outline">search & filter</v-btn>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FilterButton",
+});
+</script>
+
+<style></style>
