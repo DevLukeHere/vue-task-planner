1edc6e09bc2490de144f66d7c156f133111d5c86
Luke Wong <luke@hiredly.com>
2022-06-18 22:57:24 +0800

    Create task card

---
+++ src/components/TaskCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card tonal variant="outlined">
+    <v-card-title>Implement date range picker for application</v-card-title>
+    <v-card-text
+      >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque cumque
+      quia consequatur praesentium repudiandae nam eos cum voluptatibus corporis
+      quisquam!</v-card-text
+    >
+    <v-card-actions>
+      <v-btn icon="mdi-trash-can-outline" color="primary"></v-btn>
+      <v-btn icon="mdi-pencil" color="info"></v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TaskCard",
+
+  components: {},
+
+  methods: {},
+
+  data() {
+    return {
+      //
+    };
+  },
+});
+</script>
+
+<style></style>
