933623f65b7cffa32c64f20f39dffa20bd0b2971
Luke Wong <luke@hiredly.com>
2022-06-20 17:52:37 +0800

    Configure deployment file

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/lukewzk.github.io/",
+  base: "/vue-task-planner/",
   plugins: [vue(), vuetify({ autoImport: true })],
   resolve: {
     alias: {
